c4ecf8da0aeb7ece61990a6b121c900cbc6a98a9
GBJS0748 <fatima.moqran@orange.com>
2019-12-31 12:40:54 +0100

    update alexahellodecorator

--- alexahellodecorator.py
+++ alexahellodecorator.py
@@ -47,12 +47,29 @@ def cancel_and_stop_intent_handler(handler_input):
     handler_input.response_builder.speak(speech_text).set_card(SimpleCard("Hello World", speech_text)).set_should_end_session(True)
     return handler_input.response_builder.response
 
+#Session Ended Request handler
 @sb.request_handler(can_handle_func=is_request_type("SessionEndedRequest"))
 def session_ended_request_handler(handler_input):
     #type : (HandlerInput) -> Response
     #any cleanup logic goes here
 
     return handler_input.response_builder.response
+
+#Exception handler
+@sb.exception_handler(can_handle_func=lambda i, e: True)
+def all_exception_handler(handler_input, exception):
+    #type: (HandlerInput, Exception)-> Response
+    #Log the exception in CloudWatch logs
+    print(exception)
+
+    speech = "Sorry, I didn't get it. Can you please say it again!"
+    handler_input.response_builder.speak(speech).ask(speech)
+    return handler_input.response_builder.response
+
+#when using decorators the request handlers and exception handlers are automatically recognized by the skill buider object
+
+handler = sb.lambda_handler()
+
 
 
 
