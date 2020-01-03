46dba34de21380fa69482f39effc9c328a86dfaf
GBJS0748 <fatima.moqran@orange.com>
2020-01-03 18:18:07 +0100

    ajouts handlers et interceprtors

--- color picker.py
+++ color picker.py
@@ -54,7 +54,8 @@ def session_ended_request_handler(handler_input):
 
 @sb.request_handler(can_handle_func= is_intent_name("WhatsMyColorIntent"))
 def session_ended_request_handler(handler_input):
-    """Check if a favorite color has already been recorded in session attributes. If yes, provide the color to the user. If not, ask for favorite color."""
+    """Check if a favorite color has already been recorded in session attributes. 
+    If yes, provide the color to the user. If not, ask for favorite color."""
     #type : (HandlerInput) -> Response
     if color_slot_key in handler_input.attributes_manager.session_attributes:
         fav_color = handler_input.attributes_manager.session_attributes[color_slot_key]
@@ -62,7 +63,69 @@ def session_ended_request_handler(handler_input):
         handler_input.response_builder.set_should_end_session(True)
     else:
         speech = "je ne connais pas votre couleur préférée" + help_text
-        
+        handler_input.response_builder.ask(help_text)
+
+    handler_input.response_builder.speak(speech)
+    return handler_input.response_builder.response
+
+@sb.request_handler(can_handle_func=is_intent_name("MyColorIsIntent"))
+    def my_color_handler(handler_input):
+         """Check if color is provided in slot values. If provided, then set
+         your favorite color fromp slot value into session attributes.
+         If not, then it asks user to provide the color"""
+
+        #type : (HandlerInput) -> Response
+        slots = handler_input.request_envelope.request.intent.slots
+
+        if color_slot in slots:
+            fav_color = slots[color_slot].value
+            handler_input.attribues_manager.session_attributes[color_slot_key] = fav_color
+            speech = ("Maintenant je sais que votre couleur préférée est le {}"
+            "vous pouvez me demander quelle est votre couleur préférée en disant "
+            "quelle est ma couleur préférée".format(fav_color))
+            reprompt = ("vous pouvez me demander quelle est votre couleur préférée en disant "
+            "quelle est ma couleur préférée")
+        else:
+            speech = "Je ne suis pas sure quelle est votre couleur préférée, essayez encore"
+            #reprompt after 8 seconds if the user doesn't respond
+            reprompt = ("Je ne suis pas sure quelle est votre couleur préférée, essayez encore"
+                        "vous pouvez me dire quelle est votre couleur préférée en disant "
+                        "ma couleur préférée est rouge")
+
+        handler_input.response_builder.speak(speech).ask(reprompt)
+        return handler_input.response_builder.response
+
+#an handler that helps you handle unexpected utterances or when a customer says something that doesn't map to any intent 
+@sb.request_handler(can_handle_func = is_intent_name("AMAZON.FallbackIntent"))
+def fallback_handler(handler_input):
+    """AMAZON.FallbackIntent is only available in en-US locale.
+    This handler will not be triggered except in that locale,
+    so it is safe to deploy on any locale.
+    """
+    #type: (HandlerInput) -> Response
+    speech = (
+        "le {} ne peut pas vous aider avec ça"
+        "vous pouvez me dire quelle est votre couleur préférée en disant "
+        "ma couleur préférée est rouge".format(skill_name)
+    reprompt = ("vous pouvez me dire quelle est votre couleur préférée en disant"
+                        "ma couleur préférée est rouge")
+
+def convert_speech_to_text(ssml_speech):
+    #type : (str) -> str
+    s = SSMLStripper()
+    s.feed(ssml_speech)
+    return s.get_data()
+
+
+@sb.global_response_interceptor()
+def add_card(handler_input, response):
+    """Add a card by translating ssml text to card content"""
+    #type: (HandlerInput, response)-> None
+    response.card = SimpleCard(
+        tittle=skill_name,
+        content= convert_speech_to_text(response.output_speech.ssml))
+
+@sb.global_response_interceptor()
 
 
 
